fec62e119a03d93094f5c20872c6f04fdb68dc08
Nick Havens <nick.havens@us.bosch.com>
2018-07-27 13:47:11 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const portNumber = process.env.PORT || 3000;
+const socketIo = require('socket.io');
+const http = require('http');
+
+const server = http.Server(app);
+
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+    console.log('a user connected', socket.id);
+
+    socket.on('joinRoom', (data) => {
+        console.log('JOINING ROOM:');
+        socket.join(data.room);
+    })
+    
+    socket.on('newMsg', (data) => {
+        console.log('SENDING MSG:');
+        io.in(data.room).emit('serverMsg', {msg: data.msg})
+    })
+});
+
+app.get('/*', (req, res) => {
+    res.send('HELLO SOCKET SERVER');
+});
+
+server.listen(portNumber, () => {
+    console.log('Server started on port: ' + portNumber);
+});
